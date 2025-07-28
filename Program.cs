d714c3aebe22217048398a7be2392db23114e4a9
Marcelo Magalhães <celoxd01@gmail.com>
2025-07-27 23:36:32 -0300

    Create Project - Demonstration SqlConnection and reader commands/query in Sql Server with Docker and Azure (ADO-NET)

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False;"; 
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Connecting to database...");
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {                           // Get + Type                     // Column Number (0 is Guid, 0 is Title)
+                        Console.WriteLine($"{reader.GetGuid(0)} -  {reader.GetString(1)}");
+                    }
+                }
+            }
+            //     connection.Open();
+            //     // INSERT
+            //     // UPDATE
+            //     connection.Close();
+            //     connection.Dispose();
+            // }
+        }
+    }
+}
